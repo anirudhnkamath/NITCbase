a7f371020a5bd0cd40fdd074721c3cf17079f5c5
anirudhnkamath <anirudhnkamath@gmail.com>
2025-05-08 15:28:52 +0530

    STAGE2 [CODING]

--- Buffer/BlockBuffer.cpp
+++ Buffer/BlockBuffer.cpp
@@ -2,3 +2,43 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <stdio.h>
+
+BlockBuffer::BlockBuffer(int blockNum){
+  this->blockNum = blockNum;
+}
+
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
+
+int BlockBuffer::getHeader(struct HeadInfo* head){
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, this->blockNum);
+
+  memcpy(&head->blockType, buffer + 0, 4);
+  memcpy(&head->pblock, buffer + 4, 4);
+  memcpy(&head->lblock, buffer + 8, 4);
+  memcpy(&head->rblock, buffer + 12, 4);
+  memcpy(&head->numEntries, buffer + 16, 4);
+  memcpy(&head->numAttrs, buffer + 20, 4);
+  memcpy(&head->numSlots, buffer + 24, 4);
+  memcpy(&head->reserved, buffer + 28, 4);
+
+  return SUCCESS;
+}
+
+int RecBuffer::getRecord(union Attribute *rec, int slotNum){
+  struct HeadInfo head;
+  this->getHeader(&head);
+
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, this->blockNum);
+
+  int attrCount = head.numAttrs;
+  int slotCount = head.numSlots;
+  int recordSize = attrCount * ATTR_SIZE;
+
+  unsigned char* slotPointer = buffer + HEADER_SIZE + slotCount + (recordSize * slotNum);
+  memcpy(rec, slotPointer, recordSize);
+
+  return SUCCESS;
+}
--- main.cpp
+++ main.cpp
@@ -7,14 +7,30 @@
 
 int main(int argc, char *argv[]) {
   Disk disk_run;
- 
-  unsigned char buffer[BLOCK_SIZE];
-  for(int i=0; i<4; i++){
-    printf("\nBMAP Block %d:\n", i);
-    Disk::readBlock(buffer, i);
-    for(auto x : buffer) printf("%d", x);
-    printf("\n");
+  
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader, attrCatHeader;
+
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for(int i=0; i<relCatHeader.numEntries; i++){
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for(int j=0; j<attrCatHeader.numEntries; j++){
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if(strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0){
+        const char* attrType = (attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER) ? "NUM" : "STRING";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
   }
 
-  return 0; 
+  return 0;
 }
